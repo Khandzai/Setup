dffca1a82601e7a5baf2e06a7433da44b1bfa0d3
Khandzai <lehoangkhanhgfn@gmail.com>
2024-12-01 14:39:53 +0700

    Update NgrokSetup.sh

--- NgrokSetup.sh
+++ NgrokSetup.sh
@@ -1,1 +1,8 @@
-
+#Cài Ngrok
+wget -O ngrok.tgz "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
+#Giải nén
+tar -xf ngrok.tgz 
+#Tạo TCP
+./ngrok config add-authtoken 2p3e4fzaidMPxiUOOEFv12nLrFT_7S7HxQQiGZmcSSVJWsphn
+#Ngrok TcP
+./ngrok tcp 5900 
