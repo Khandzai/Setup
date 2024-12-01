77b7b913caaa0c63a6f8fcdc0540689f59a4fd49
Khandzai <lehoangkhanhgfn@gmail.com>
2024-12-01 14:00:14 +0700

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -6,8 +6,6 @@ sudo apt install qemu-kvm -y
 wget -O win11.iso "https://computernewb.com/isos/windows/Windows11_InsiderPreview_Client_x64_en-us_26040.iso"
 #Install Driver
 wget -O driver.iso "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.262-2/virtio-win-0.1.262.iso"
-#Run Kvm
-sudo kvm -cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm -smp 4,cores=4 -M q35,usb=on -device usb-tablet -m 8G -device virtio-balloon-pci -vga virtio -net nic,netdev=n0,model=virtio-net-pci -netdev user,id=n0,hostfwd=tcp::3389-:3389 -boot c -device virtio-serial-pci -device virtio-rng-pci -enable-kvm -drive file=/dev/sdc,format=raw,if=none,id=nvme0 -device nvme,drive=nvme0,serial=deadbeaf1,num_queues=8 -monitor stdio -drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd -uuid e47ddb84-fb4d-46f9-b531-14bb15156336 -vnc :0 -drive file=driver.iso,media=cdrom -drive file=win11.iso,media=cdrom
 #Cài Ngrok
 wget -O ngrok.tgz "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
 #Giải nén
@@ -16,4 +14,5 @@ tar -xf ngrok.tgz
 ./ngrok config add-authtoken 2p3e4fzaidMPxiUOOEFv12nLrFT_7S7HxQQiGZmcSSVJWsphn
 #Ngrok TcP
 ./ngrok tcp 5900 
-echo"Đăng Nhập Và Sử Dụng"
+#Run KVM
+sudo kvm -cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm -smp 4,cores=4 -M q35,usb=on -device usb-tablet -m 8G -device virtio-balloon-pci -vga virtio -net nic,netdev=n0,model=virtio-net-pci -netdev user,id=n0,hostfwd=tcp::3389-:3389 -boot c -device virtio-serial-pci -device virtio-rng-pci -enable-kvm -drive file=/dev/sdc,format=raw,if=none,id=nvme0 -device nvme,drive=nvme0,serial=deadbeaf1,num_queues=8 -monitor stdio -drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd -uuid e47ddb84-fb4d-46f9-b531-14bb15156336 -vnc :0 -drive file=driver.iso,media=cdrom -drive file=win11.iso,media=cdrom
